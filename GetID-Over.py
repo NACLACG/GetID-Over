5f8536199e2d77df0a2ca987ae73a4c764cd8db4
NACLACG <57287709+NACLACG@users.noreply.github.com>
2023-05-23 15:03:16 +0800

    Update GetID-Over.py

--- GetID-Over.py
+++ GetID-Over.py
@@ -2,15 +2,16 @@ import sys
 import random
 import re
 import pyperclip
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel
 from OWO import Ui_GetID
 
 IDfile = open('IDS.txt')
 with open('IDS.txt', 'r') as f:
     ID = f.read()
-print(ID)
-# 初始化GetID字符串数组
+# 初始化ID字符串数组
 GetID = []
+BanID = []
+Temper = ""
 IDlong = 0
 Ltmp = ""
 # 处理IDS.txt文件 将每一行的点歌号存入GetID数组
@@ -21,13 +22,10 @@ while IDlong >= 0:
     else:
         GetID.append(Ltmp)
         IDlong += 1
-IDfile.close()
 IDfile = open('BAN.txt')
 with open('BAN.txt', 'r') as f:
-    ID = f.read()
-print(ID)
-# 初始化GetID字符串数组
-BanID = []
+    Ban = f.read()
+# 初始BanID字符串数组
 IDlong = 0
 Ltmp = ""
 # 处理IDS.txt文件 将每一行的点歌号存入GetID数组
@@ -58,16 +56,36 @@ class MyUI(QWidget, Ui_GetID):
             Temp = match.group(1)
             # 如果ID包含Temp变量的字符串 随机输出GetID数组中的一个元素到剪贴板
             if Temp in ID:
-                pyperclip.copy(random.choice(GetID))
+                Temper = random.choice(GetID)
+                pyperclip.copy(Temper)
             # 如果ID不包含Temp变量的字符串 将字符串写入IDS.txt文件 并添加到GetID数组中
             else:
                 with open('IDS.txt', 'a') as f:
                     new_id = '点歌 ' + Temp
                     f.write(new_id + '\n')
                     GetID.append(new_id)
+                with open('BAN.txt', 'a') as f:
+                    new_id = '点歌 ' + Temp
+                    f.write(new_id + '\n')
+                    BanID.append(new_id)
+                IDfile.close()
                 pyperclip.copy(new_id)
         else:
-            pyperclip.copy(random.choice(GetID))
+            while True:
+                Temper = random.choice(GetID)
+                self.showget.setText(Temper.replace("\n",""))
+                if Temper in Ban:
+                    self.showready.setText(Temper.replace("\n",""))
+                    print(Temper)
+                    continue
+                else:
+                    pyperclip.copy(Temper)
+                    with open('BAN.txt', 'a') as f:
+                        f.write(Temper)
+                        BanID.append(Temper)
+                    IDfile.close()
+                    self.showget.setText(Temper.replace("\n",""))
+                    break
 
 if __name__ == '__main__':
     app = QApplication([])
